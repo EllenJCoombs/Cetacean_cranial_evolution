6d0cf8c9d590c41662c42714fd85c14ea5bba0f2
EllenJCoombs <ellen.coombs.14@ucl.ac.uk>
2022-04-29 03:46:37 +0100

    Update Binary_ASCII_ply.R

--- code/Binary_ASCII_ply.R
+++ code/Binary_ASCII_ply.R
@@ -8,12 +8,10 @@
 
 #run this script before reading in your .pts files 
 
-#this makes nice 3D outputs of the files for publications as well (such as the M.Mirus) - SEE BOTTOM
+#this makes nice 3D outputs of the files for publications as well - SEE BOTTOM
 #script for converting binary to ASCII .ply files
 #rm(list=ls())
 
-install.packages("Rvcg")
-install.packages("rgl")
 library(Rvcg)
 library(rgl)
 
@@ -46,4 +44,13 @@ for (i in 1:length(meshlist)){
 #Set the directory to the one with the .ply in it 
 checkLM(newpts,path="./ply/",suffix=".ply",pt.size=1,render="s",alpha=1)
 
+#For taking a snapshot for publications 
+#install.packages('Morpho')
+library(Morpho)
+library(rgl)
 
+#don't forget .ply
+Lissodelphis=ply2mesh(file="X:xxxxx/ply/Lissodelphis borealis USNM 550188.ply")
+
+shade3d(Lissodelphis, col="white") #"white" or bone1 (don't use the "" for the latter) can use bone2 or bone3 also
+rgl.snapshot(filename = "X:xxxxx/ply/Lissodelphis borealis USNM 550188.png") #the object name and then underscore followed by the new file na
