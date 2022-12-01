43ec258415193f3a34d8ee5c44e0cee079071bb7
EllenJCoombs <ellen.coombs.14@ucl.ac.uk>
2022-12-01 09:27:33 -0500

    Update BayesTraits_rates_phylo.R

--- code/BayesTraits_rates_phylo.R
+++ code/BayesTraits_rates_phylo.R
@@ -31,7 +31,7 @@ add_rjpp_to_tree <- function(rjpp_out){
   return(treedata)
 }
 
-#You can do cool things like this 
+#This is where you pull in your rates from BayesTraits (here, I am looking at the whole whale skull - you could also just put in the rates for the nasal or premax etc
 tree_2_BTraits<-BTRTools::rjpp(rjlog = "whole_var.txt.VarRates.txt",
                                rjtrees = "whole_var.txt.Output.trees", 
                                tree = treeWHOLE) #this is your time scaled tree that was used to input into bayestraits
@@ -44,25 +44,25 @@ save(tree_2_BTraits, file = 'tree_2_BTraits.R')
 tree_2_w_data <- add_rjpp_to_tree(tree_2_BTraits)
 p<-ggtree(tree_2_w_data, aes(color = log(meanRate)), size=1)
 
-#Create a colour palette 
+#Create a colour palette for the rates
 colspal<-colorRampPalette(c("blue","cyan","yellow","red"))
 
 #check the ranges of the mean rates 
 ggplot(tree_2_w_data@extraInfo, aes(x=log(meanRate)))+geom_histogram()
 
-#plot
+#plot the rates on the tree
 tree_2_w_data <- add_rjpp_to_tree(tree_2_BTraits)
 threshold <- .4 # the minimum posterior probability you want to plot a symbol for 
 p<-ggtree(tree_2_w_data, aes(color = log(meanRate)), size=2)+ #this is where you can add  layout="fan" if you want a fan 
-  scale_colour_stepsn(breaks = seq(from=-1,to=6,by=.2), colours = colspal(100))+
-  #theme(legend.position="top")+
+  scale_colour_stepsn(breaks = seq(from=-1,to=6,by=.2), colours = colspal(100))+ #colspal is how many cols you want (the range)
+  #theme(legend.position="top")+ #play around with the legend
   theme(legend.position=c(.32,.83),legend.direction = "horizontal",legend.box.background = element_rect(colour = "black",size =1))+
   scale_size(range = c(1,2))+ 
   labs(title="Cetacean  skull rates",
        color="log(Rate)")+
   geom_nodepoint(aes(subset=ppRate>threshold, size = ppRate),color='black',fill="grey", shape=24)+
   geom_tiplab(label= sub("_", " ",tree_2_w_data@phylo$tip.label), size=2, color = "black")+
-  coord_cartesian(xlim = c(-50, 9), #you have to fiddle with these values to get your tip labels to show. the first value should be just before your root time, second value pads out space for tip labels
+  coord_cartesian(xlim = c(-50, 9), #you have to fiddle with these values to get your tip labels to show. The first value should be just before your root time, second value pads out space for tip labels
                   ylim = c(-2, 200), #first value makes room for geo timescale, second value is vertical space and should be a few more than your number of tips
                   expand = FALSE) +
   scale_x_continuous(breaks=-epochs$max_age[c(1:6)], labels=epochs$max_age[c(1:6)]) + 
@@ -70,4 +70,5 @@ p<-ggtree(tree_2_w_data, aes(color = log(meanRate)), size=2)+ #this is where you
 p <- revts(p) 
 ptree2 <- gggeo_scale(p, dat = "epochs", neg = FALSE, center_end_labels = TRUE, height = unit(1, "line"),  size=3)
 
+#Plot!
 ptree2
